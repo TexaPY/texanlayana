de3f9c4ff174a95efd81ec97de9d1575916149db
TeXa <abdullahtambas37@gmail.com>
2024-10-28 00:21:18 +0300

    offf

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,33 +1,24 @@
-// Firebase Messaging'i içe aktar
-importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js"
-);
+importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js');
 
-// Firebase'i yapılandır
-const firebaseConfig = {
-  apiKey: "AIzaSyCfbStb3I1qZYqYKHrP_2wcGvOwUi4GrRs",
-  authDomain: "gigi-67cd0.firebaseapp.com",
-  databaseURL: "https://gigi-67cd0-default-rtdb.firebaseio.com",
-  projectId: "gigi-67cd0",
-  storageBucket: "gigi-67cd0.appspot.com",
-  messagingSenderId: "813483059670",
-  appId: "1:813483059670:web:7c03147ba03344098b1c77",
-  measurementId: "G-55Y887Q50X",
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+    apiKey: "AIzaSyCfbStb3I1qZYqYKHrP_2wcGvOwUi4GrRs",
+    authDomain: "gigi-67cd0.firebaseapp.com",
+    databaseURL: "https://gigi-67cd0-default-rtdb.firebaseio.com",
+    projectId: "gigi-67cd0",
+    storageBucket: "gigi-67cd0.appspot.com",
+    messagingSenderId: "813483059670",
+    appId: "1:813483059670:web:7c03147ba03344098b1c77",
+    measurementId: "G-55Y887Q50X"
+});
 
 const messaging = firebase.messaging();
 
-// Arka planda bildirimleri dinleme
-messaging.onBackgroundMessage(function (payload) {
-  console.log("Arka planda mesaj alındı: ", payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon,
-  };
+messaging.onBackgroundMessage((payload) => {
+    console.log('Arka planda mesaj alındı:', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
