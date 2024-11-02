6863d939b75c148cc552be099c90a144a798a48a
TeXa <abdullahtambas37@gmail.com>
2024-11-02 10:59:01 +0300

    asdasdasd

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,7 +5,7 @@ importScripts(
   "https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging-compat.js"
 );
 
-// Initialize Firebase in the service worker
+// Firebase'i başlat
 firebase.initializeApp({
   apiKey: "AIzaSyD3JwJRG1_xbyNYkXs_kJBb5pc4XzlH9jQ",
   authDomain: "ezgigi-5f883.firebaseapp.com",
@@ -17,21 +17,25 @@ firebase.initializeApp({
   measurementId: "G-G4YB4MMWBP",
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
+// Firebase Messaging örneğini al
 const messaging = firebase.messaging();
 
+// Arka planda mesaj alındığında
 messaging.onBackgroundMessage((payload) => {
   console.log(
     "[firebase-messaging-sw.js] Received background message ",
     payload
   );
-  // Customize notification here
-  const notificationTitle = "Background Message Title";
+
+  // Günlük bildirim için içerik ayarları
+  const notificationTitle = "Bugüne Özel Şarkını dinledin mi?";
+  const notificationBody = payload.notification?.body || "Opa'nın Gigi için seçtiği günün şarkısını dinledin mi?";
   const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
+    body: notificationBody,
+    icon: "/assets/images/apos.ico", // İkon yolunu buraya ekleyin
+    image: payload.notification?.image || "https://texa.anlayana.com/assets/images/notify.png", // İsterseniz görsel ekleyebilirsiniz
   };
 
+  // Bildirimi göster
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
