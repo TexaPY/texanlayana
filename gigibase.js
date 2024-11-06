403159a098afa899a0c19da652380197099d2338
TeXa <abdullahtambas37@gmail.com>
2024-11-07 00:57:36 +0300

    trial

--- gigibase.js
+++ gigibase.js
@@ -76,8 +76,13 @@ if ("serviceWorker" in navigator) {
     .register("/firebase-messaging-sw.js")
     .then((registration) => {
       console.log("Service Worker registered with scope:", registration.scope);
-      // Servis çalışanı kaydedildiğinde bildirim izni isteyin
-      requestNotificationPermission();
+      // Servis çalışanı kaydedildikten sonra bildirim izni isteyin
+      registration.pushManager.getSubscription().then((subscription) => {
+        if (!subscription) {
+          // Abonelik yoksa izni iste
+          requestNotificationPermission();
+        }
+      });
     })
     .catch((error) => {
       console.error("Service Worker registration failed:", error);
